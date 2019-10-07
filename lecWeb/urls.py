ffe763b368b2f402016426a19c18216ee38b70c2
zangbaby <37652800+zangbaby@users.noreply.github.com>
2019-10-07 22:19:13 +0800

    Update urls.py
    臧子晗 添加了soluation的路径

--- lecWeb/urls.py
+++ lecWeb/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^index/',include('main.urls')),
+    url(r'^soluation/', include('web.urls')),
 ]
