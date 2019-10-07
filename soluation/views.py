72c5c87e7fc0b4635daf838aa83549c9898b6e3d
zangbaby <37652800+zangbaby@users.noreply.github.com>
2019-10-07 22:26:27 +0800

    添加solution页面跳转函数

--- soluation/views.py
+++ soluation/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def solutions_Integrated_commercial(request):
+    return render(request, 'soluation/solutions_Integrated_commercial.html')
