24ba07b82908bf01b4f1b1a0307ebb5bfd34d3e9
tarena <a792721538@outlook.com>
2019-10-10 22:10:20 +0800

    史飞飞 2019年10月10日22:09:51

--- soluation/models.py
+++ soluation/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+# Create your models here.
+class Users(models.Model):
+    mobilenum = models.CharField(max_length=32,verbose_name='手机号',null=True)
+    createDate = models.DateTimeField(null=True,auto_now_add=True)
--- soluation/urls.py
+++ soluation/urls.py
@@ -5,4 +5,12 @@ from soluation import views
 urlpatterns = [
     url(r'^test$', views.test),
     url(r'^index$', views.solutions_Integrated_commercial),
+    #soluation首页
+    #http://127.0.0.1:8000/soluation/conten1
+    url(r'^conten1$',views.conten1),
+    # http://127.0.0.1:8000/soluation/reg
+    url(r'^reg$', views.reg),
+    # 负责存储信息
+    # http://127.0.0.1:8000/soluation/server
+    url(r'^server$', views.server),
 ]
