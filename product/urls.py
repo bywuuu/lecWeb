2e04f072d95a26964cab5983a47c5ffdb3196fc4
lofues <junfuwang@163.com>
2019-10-19 17:12:09 +0800

    王俊夫 10月19日 17:11 完善了商品的搜索功能，添加了个人信息的页面与修改功能,

--- product/urls.py
+++ product/urls.py
@@ -6,5 +6,10 @@ urlpatterns = [
     url(r'^register$', views.reg_view),
     url(r'^login$', views.log_view),
     url(r'^list$', views.product_list_view),
-    url(r'^all$', views.product_all)
+    url(r'^all$', views.product_all),
+    url(r'^buy/(?P<product_id>\d+)$', views.product_buy),
+    url(r'^change_info$', views.change_info),
+    url(r'^change_info/(?P<username>\w+)$', views.change_info),
+    url(r'^quit_login/(?P<username>\w+)$', views.quit_login),
+    url(r'^add_money/(?P<username>\w+)$', views.add_money)
 ]
