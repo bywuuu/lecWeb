e6d3576e052397019fd6e6ebc0dc3e7ae1d4bf34
xuyue <xuyue6626@126.com>
2019-10-23 22:30:27 +0800

    徐月 添加图片左右滑功能 2019 10 21

--- skill/models.py
+++ skill/models.py
@@ -2,5 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Tec(models.Model):
-    like = models.CharField(verbose_name='喜欢',max_length=10)
-    dislike = models.CharField(verbose_name='不喜欢',max_length=10)
+    img_id = models.CharField(verbose_name='图片id',max_length=10,default=None)
+    username = models.CharField(verbose_name='用户名',max_length=32,default=None)
+    like=models.BooleanField(verbose_name='like/dislike')
+
